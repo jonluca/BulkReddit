dbe2b470f6d9c4899321f7b2b24bd84f67c10bd2
JonLuca DeCaro <jdecaro@usc.edu>
2017-06-13 17:06:03 -0700

    init html work

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const snoowrap = require('snoowrap');
+var config = require('./config.js');
 
 // NOTE: The following examples illustrate how to use snoowrap. However, hardcoding
 // credentials directly into your source code is generally a bad idea in practice (especially
@@ -8,11 +9,17 @@ const snoowrap = require('snoowrap');
 // Create a new snoowrap requester with OAuth credentials.
 // For more information on getting credentials, see here: https://github.com/not-an-aardvark/reddit-oauth-helper
 const r = new snoowrap({
-    userAgent: 'put your user-agent string here',
-    clientId: 'cO_Tk2b60ij01Q',
-    clientSecret: 'oM2ZP_RFxCYvVOR9zL3UxZ-h8yU',
-    refreshToken: '11560010-hX54UQPDOKnRlL0eIXVfJexrcos'
+    userAgent: 'BulkReddit/1.0',
+    clientId: config.secret.client_id,
+    clientSecret: config.secret.secret,
+    refreshToken: config.secret.refresh
 });
 
-var posts = r.getSubreddit("talesfromtechsupport").getHot();
-console.log(posts);
+r.getSubreddit("talesfromtechsupport").getHot().then(function(data) {
+    pbcopy(data);
+});
+
+function pbcopy(data) {
+    var proc = require('child_process').spawn('pbcopy');
+    proc.stdin.write(JSON.stringify(data)); proc.stdin.end();
+}
