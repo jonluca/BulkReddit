742aae312e0e4218a1fe3cb1ebd9f71fb706ac4a
JonLuca DeCaro <jdecaro@usc.edu>
2017-06-14 14:40:01 -0700

    init ajax code

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,43 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
 }
+//extension to jQuery for animate.css
+$.fn.extend({
+    animateCss: function(animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
+$(document).ready(function() {
+    $('#txt').click(function() {
+        getTxt();
+    });
+
+    $('#pdf').click(function() {
+        getPdf();
+    });
+});
+
+function getTxt() {
+    $('#txt').animateCss('pulse');
+    $.ajax({
+        method: 'POST',
+        url: "/BulkReddit/download",
+        type: 'json',
+        data: {
+            checked: checkArray
+        },
+        success: showNewImage,
+        error: function(data, code, jqXHR) {
+            $(".loader").css("display", "none");
+            $("#info").text("Error! Please try again.");
+        }
+    });
+}
+
+function getPdf() {
+    $('#pdf').animateCss('pulse');
+}
