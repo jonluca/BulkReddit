58ab6f680de0518cdfc23e7af935cdc1a3d5edb6
JonLuca DeCaro <jdecaro@usc.edu>
2017-06-13 17:12:48 -0700

    helpers

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ var app = express();
 var path = require('path');
 var request = require('request');
 
-// NOTE: The following examples illustrate how to use snoowrap. However, hardcoding
-// credentials directly into your source code is generally a bad idea in practice (especially
-// if you're also making your source code public). Instead, it's better to either (a) use a separate
-// config file that isn't committed into version control, or (b) use environment variables.
+const prefix = "/BulkReddit";
+app.use(prefix + '/', express.static(path.join(__dirname, 'public')));
 
-// Create a new snoowrap requester with OAuth credentials.
-// For more information on getting credentials, see here: https://github.com/not-an-aardvark/reddit-oauth-helper
 const r = new snoowrap({
     userAgent: 'BulkReddit/1.0',
     clientId: config.secret.client_id,
@@ -19,10 +15,19 @@ const r = new snoowrap({
     refreshToken: config.secret.refresh
 });
 
-r.getSubreddit("talesfromtechsupport").getHot().then(function(data) {
-    pbcopy(data);
+//Force refresh API sooner than 5 minutes
+app.get(prefix + "/", function(req, res) {
+    res.status(200);
+    res.render("landing.ejs");
+    r.getSubreddit("talesfromtechsupport").getHot().then(function(data) {
+        pbcopy(data);
+    });
 });
 
+
+
+
+//Function to copy to clipboard - mac
 function pbcopy(data) {
     var proc = require('child_process').spawn('pbcopy');
     proc.stdin.write(JSON.stringify(data)); proc.stdin.end();
