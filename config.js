ef0c31cdbae39766976b191c497a94feefd92e9d
jonluca <jdecaro@usc.edu>
2018-03-22 18:02:57 +0000

    config

--- config.js
+++
@@ -1,7 +0,0 @@
-var secret = {
-    client_id: "cO_Tk2b60ij01Q",
-    refresh: "11560010-hX54UQPDOKnRlL0eIXVfJexrcos",
-    secret: "oM2ZP_RFxCYvVOR9zL3UxZ-h8yU"
-};
-
-module.exports.secret = secret;
